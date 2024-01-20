3b54f79dd7563e9bec1d194b4179baaeb655ec71
YKPENDAL <yashpendal18@gnu.ac.in>
2024-01-20 16:39:19 +0530

    test14

--- testup.sh
+++ testup.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-# Update the package list
 sudo apt update
 
-# Update the package list
 sudo apt upgrade
 
-# Install Nginx
+# nginx install
 sudo apt install -y nginx
 
-# Start Nginx
 sudo systemctl start nginx
 
-# Enable Nginx to start on boot
 sudo systemctl enable nginx
 
-# Allow Nginx through the firewall (if applicable)
-#sudo ufw allow 'Nginx HTTP'
-
-# Display Nginx status
 sudo systemctl status nginx
 
+
+# mysql install
 sudo apt-get install -y mysql-server
             
 sudo systemctl start mysql
             
 sudo systemctl enable mysql
 
+sudo systemctl status mysql
