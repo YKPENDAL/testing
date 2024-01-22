bca3de9333cb7ecd3a118ffc70f518b3cd8dbcfd
YKPENDAL <yashpendal18@gnu.ac.in>
2024-01-22 13:28:02 +0530

    Final Code_2

--- script.sh
+++ script.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
 # Function to run commands with and handle password input
+run_with_sudo() {
+  echo "Executing: sudo $*"
+  sudo -S <<< "$PASSWORD" "$@"
+}
+
+# Set your sudo password
+PASSWORD='yash@12345'
+
 
 # Update and upgrade
-apt update
-apt upgrade -y
-
+run_with_sudo apt update
+run_with_sudo apt upgrade -y
+ 
 # Nginx installation
-apt install -y nginx
-systemctl start nginx
-systemctl enable nginx
-systemctl status nginx
+run_with_sudo apt install -y nginx
+run_with_sudo systemctl start nginx
+run_with_sudo systemctl enable nginx
+run_with_sudo systemctl status nginx
 
 # MySQL installation
-apt-get install -y mysql-server
-systemctl start mysql
-systemctl enable mysql
-systemctl status mysql
+run_with_sudo apt-get install -y mysql-server
+run_with_sudo systemctl start mysql
+run_with_sudo systemctl enable mysql
+run_with_sudo systemctl status mysql
 
 # JDK installation
-apt-get install -y openjdk-11-jdk
+run_with_sudo apt-get install -y openjdk-11-jdk
 java -version
