258a1a25002f052662fbe84aa21de974d3418fef
YKPENDAL <yashpendal18@gnu.ac.in>
2024-01-22 13:25:04 +0530

    Final Code_1

--- script.sh
+++ script.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# Function to run commands with sudo and handle password input
+# Function to run commands with and handle password input
 
 # Update and upgrade
-sudo apt update
-sudo apt upgrade -y
+apt update
+apt upgrade -y
 
 # Nginx installation
-sudo apt install -y nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
-sudo systemctl status nginx
+apt install -y nginx
+systemctl start nginx
+systemctl enable nginx
+systemctl status nginx
 
 # MySQL installation
-sudo apt-get install -y mysql-server
-sudo systemctl start mysql
-sudo systemctl enable mysql
-sudo systemctl status mysql
+apt-get install -y mysql-server
+systemctl start mysql
+systemctl enable mysql
+systemctl status mysql
 
 # JDK installation
-sudo apt-get install -y openjdk-11-jdk
+apt-get install -y openjdk-11-jdk
 java -version
