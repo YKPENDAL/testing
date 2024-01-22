73fde3bed9f86db7a9184d3b37b868ac3951eb05
YKPENDAL <yashpendal18@gnu.ac.in>
2024-01-22 13:33:40 +0530

    Final Code_3

--- script.sh
+++ script.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# Function to run commands with and handle password input
+# Function to run commands with sudo and handle password input
 run_with_sudo() {
+  if [ -z "$PASSWORD" ]; then
+    echo "Error: PASSWORD variable is not set"
+    exit 1
+  fi
+
   echo "Executing: sudo $*"
-  sudo -S <<< "$PASSWORD" "$@"
+  sudo -S -i <<< "$PASSWORD" "$@"
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to execute command with sudo"
+    exit 1
+  fi
 }
 
 # Set your sudo password
 PASSWORD='yash@12345'
-
 
 # Update and upgrade
 run_with_sudo apt update
@@ -27,5 +35,5 @@ run_with_sudo systemctl enable mysql
 run_with_sudo systemctl status mysql
 
 # JDK installation
-run_with_sudo apt-get install -y openjdk-11-jdk
+run_with_sudo apt-get install -y openjdk-18-jdk
 java -version
