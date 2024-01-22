794ea15f635779627fa46bcc8124ac804c6b1868
YKPENDAL <yashpendal18@gnu.ac.in>
2024-01-22 15:55:09 +0530

    Final Code_22

--- script.sh
+++ script.sh
@@ -20,25 +20,3 @@ EOF
 
 
 
-# Display the current working directory
-echo "Current directory: $(pwd)"
-
-# Update and upgrade
-sudo apt update
-sudo apt upgrade -y
- 
-# Nginx installation
-sudo apt install -y nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
-sudo systemctl status nginx
-
-# MySQL installation
-sudo apt-get install -y mysql-server
-sudo systemctl start mysql
-sudo systemctl enable mysql
-sudo systemctl status mysql
-
-# JDK installation
-sudo apt-get install -y openjdk-18-jdk
-java -version
