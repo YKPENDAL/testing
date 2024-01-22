4bfe8fd888f0c42825a65b1c162c6bc8298ae209
YKPENDAL <yashpendal18@gnu.ac.in>
2024-01-22 15:50:59 +0530

    Final Code_20

--- script.sh
+++ script.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# Your GitHub Secrets
+EC2_INSTANCE_IP=$HOST_DNS
+KEY_PATH=$EC2_SSH_KEY
+DESTINATION_PATH="/path/to/destination/"
+BASH_SCRIPT_PATH="/path/to/your/bash.sh"
+
+# Connect to the EC2 instance using SSH and upload the bash script
+ssh -i "$KEY_PATH" ubuntu@"$EC2_INSTANCE_IP" << EOF
+    # Create the destination directory if it doesn't exist
+    mkdir -p "$DESTINATION_PATH"
+    
+    # Upload the bash script
+    scp -i "$KEY_PATH" "$BASH_SCRIPT_PATH" ubuntu@"$EC2_INSTANCE_IP":"$DESTINATION_PATH"
+EOF
+
+
+
 # Display the current working directory
 echo "Current directory: $(pwd)"
 
